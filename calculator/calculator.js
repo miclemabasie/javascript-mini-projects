75af86dd585edb747b19c032570e8607a0e18081
Miclem <miclemabasie3@gmail.com>
2023-05-11 22:00:34 +0100

    mutting and umutting buttons

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,11 +3,13 @@ const input1 = document.getElementById("num1"),
 input2 = document.getElementById("num2"),
 results = document.getElementById("results")
 
+
 // Get the all the buttons as a node list
 const buttons = document.querySelectorAll(".button")
 
 // loop over the list of button and attatch an event to each button
 buttons.forEach(button => {
+    button.disabled = true;
     button.addEventListener("click", function() {
         calculate(button.id)
     })
@@ -32,5 +34,22 @@ function calculate(operation){
     }else if(res == 0) {
         results.innerHTML = res;
     }
-
 }
+
+
+function activateBtns(element, other){
+    element.addEventListener("keydown", function() {
+        if(parseInt(element.value) && parseInt(other.value)){
+            buttons.forEach(button => {
+                button.disabled = false;
+            });
+        }else{
+            buttons.forEach(button => {
+                button.disabled = true;
+            });
+        }
+    })
+}
+
+activateBtns(input1, input2)
+activateBtns(input2, input1)
